c732899d1ebb98b0d63862d79b8f9c6d8ad95522
erfannvb <erfannavab@gmail.com>
2024-04-22 00:52:59 +0330

    before saving the user, its password will be encoded

--- src/main/java/nvb/dev/kavoshsampleproject/service/impl/UserServiceImpl.java
+++ src/main/java/nvb/dev/kavoshsampleproject/service/impl/UserServiceImpl.java
@@ -6,6 +6,7 @@ import nvb.dev.kavoshsampleproject.exception.UserNotFoundException;
 import nvb.dev.kavoshsampleproject.exception.UsernameExistsException;
 import nvb.dev.kavoshsampleproject.repository.UserRepository;
 import nvb.dev.kavoshsampleproject.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,10 +16,12 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public User saveUser(User user) {
         if (usernameExists(user.getUsername())) throw new UsernameExistsException(user.getUsername());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
