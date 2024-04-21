060a92b2ddc75d07b832bd1abc999b9655f1a387
erfannvb <erfannavab@gmail.com>
2024-04-21 19:25:41 +0330

    Initial commit

---
+++ src/main/java/nvb/dev/kavoshsampleproject/KavoshSampleProjectApplication.java
@@ -0,0 +1,13 @@
+package nvb.dev.kavoshsampleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KavoshSampleProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KavoshSampleProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nvb/dev/kavoshsampleproject/KavoshSampleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package nvb.dev.kavoshsampleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KavoshSampleProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
