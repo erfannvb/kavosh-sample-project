fe67740d322fe7e1337d80685be42f62eaacab7f
erfannvb <erfannavab@gmail.com>
2024-04-21 20:37:54 +0330

    implements UserDetails interface

---
+++ src/main/java/nvb/dev/kavoshsampleproject/config/UserInfoConfig.java
@@ -0,0 +1,51 @@
+package nvb.dev.kavoshsampleproject.config;
+
+import lombok.RequiredArgsConstructor;
+import nvb.dev.kavoshsampleproject.entity.UserInfo;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class UserInfoConfig implements UserDetails {
+
+    private final UserInfo userInfo;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return userInfo.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return userInfo.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
