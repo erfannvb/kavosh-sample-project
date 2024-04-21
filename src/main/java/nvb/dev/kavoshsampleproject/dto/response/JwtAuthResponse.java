011423782585ea5198c98866b47d9f643e301bb9
erfannvb <erfannavab@gmail.com>
2024-04-21 20:32:26 +0330

    returns token

---
+++ src/main/java/nvb/dev/kavoshsampleproject/dto/response/JwtAuthResponse.java
@@ -0,0 +1,12 @@
+package nvb.dev.kavoshsampleproject.dto.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class JwtAuthResponse {
+
+    private String token;
+
+}
