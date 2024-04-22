6a85879aef9100099463c73d525dd998d1d93fec
erfannvb <erfannavab@gmail.com>
2024-04-22 09:15:21 +0330

    create Post entity

---
+++ src/main/java/nvb/dev/kavoshsampleproject/entity/Post.java
@@ -0,0 +1,25 @@
+package nvb.dev.kavoshsampleproject.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tbl_data")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "title cannot be blank.")
+    private String title;
+
+}
