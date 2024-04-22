b339c1280e4bbe7a0af53b84a7d859fe9ae72820
erfannvb <erfannavab@gmail.com>
2024-04-22 09:17:14 +0330

    implement post repository

---
+++ src/main/java/nvb/dev/kavoshsampleproject/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package nvb.dev.kavoshsampleproject.repository;
+
+import nvb.dev.kavoshsampleproject.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
